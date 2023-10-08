e412067fb2f734118333d2bd87e3df960919b493
aybe <aybe@users.noreply.github.com>
2023-10-08 22:04:08 +0200

    Clean up SectorMode2Form1

--- WipeoutInstaller/WorkInProgress/SectorMode2Form1.cs
+++ WipeoutInstaller/WorkInProgress/SectorMode2Form1.cs
@@ -1,12 +1,25 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace WipeoutInstaller.WorkInProgress;
 
+[SuppressMessage("ReSharper", "UnassignedField.Global")]
 public unsafe struct SectorMode2Form1
 {
-    public fixed byte Sync[12];
+    private const int SyncSize = 12;
+
+    public fixed byte Sync[SyncSize];
+
     public fixed byte Header[4];
+
     public fixed byte SubHeader[8];
-    public fixed byte UserData[2048];
-    public fixed byte EDC[4];
+
+    public fixed byte UserData[UserDataSize];
+
+    private const int UserDataSize = 2048;
+
+    public fixed byte Edc[4];
+
     public fixed byte ParityP[172];
+
     public fixed byte ParityQ[104];
 }
