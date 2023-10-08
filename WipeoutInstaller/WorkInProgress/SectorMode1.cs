99887d2ec470b0606ba0e5aa3bc1386bd0558da1
aybe <aybe@users.noreply.github.com>
2023-10-08 22:14:29 +0200

    Clean up SectorMode1

--- WipeoutInstaller/WorkInProgress/SectorMode1.cs
+++ WipeoutInstaller/WorkInProgress/SectorMode1.cs
@@ -1,12 +1,35 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace WipeoutInstaller.WorkInProgress;
 
 public unsafe struct SectorMode1
+[SuppressMessage("ReSharper", "UnassignedField.Global")]
 {
-    public fixed byte Sync[12];
-    public fixed byte Header[4];
-    public fixed byte UserData[2048];
-    public fixed byte EDC[4];
-    public fixed byte Intermediate[8];
-    public fixed byte ParityP[172];
-    public fixed byte ParityQ[104];
+    private const int SyncSize = 12;
+
+    public fixed byte Sync[SyncSize];
+
+    private const int HeaderSize = 4;
+
+    public fixed byte Header[HeaderSize];
+
+    private const int UserDataSize = 2048;
+
+    public fixed byte UserData[UserDataSize];
+
+    private const int EdcSize = 4;
+
+    public fixed byte Edc[EdcSize];
+
+    private const int IntermediateSize = 8;
+
+    public fixed byte Intermediate[IntermediateSize];
+
+    private const int PParitySize = 172;
+
+    public fixed byte PParity[PParitySize];
+
+    private const int QParitySize = 104;
+
+    public fixed byte QParity[QParitySize];
 }
