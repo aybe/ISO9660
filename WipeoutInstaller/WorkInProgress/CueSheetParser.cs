7e988ca4ee366c7b2de4d9b117262e06211245b6
aybe <aybe@users.noreply.github.com>
2023-10-09 21:25:36 +0200

    Extract parsing logic into methods

--- WipeoutInstaller/WorkInProgress/CueSheetParser.cs
+++ WipeoutInstaller/WorkInProgress/CueSheetParser.cs
@@ -8,6 +8,22 @@ namespace WipeoutInstaller.WorkInProgress;
 [SuppressMessage("ReSharper", "StringLiteralTypo")]
 public static partial class CueSheetParser
 {
+    private static readonly IReadOnlyDictionary<Regex, CueSheetHandler> Handlers =
+        new Dictionary<Regex, CueSheetHandler>
+            {
+                { FileRegex(), FileHandler },
+                { CatalogRegex(), CatalogHandler },
+                { FlagsRegex(), FlagsHandler },
+                { IndexRegex(), IndexHandler },
+                { PerformerRegex(), PerformerHandler },
+                { PreGapRegex(), PreGapHandler },
+                { RemRegex(), RemHandler },
+                { TitleRegex(), TitleHandler },
+                { TrackRegex(), TrackHandler },
+                { IsrcRegex(), IsrcHandler }
+            }
+            .AsReadOnly();
+
     public static CueSheet Parse(Stream stream)
     {
         using var reader = new StreamReader(stream, Encoding.Default, true);
@@ -30,188 +46,27 @@ public static partial class CueSheetParser
                 continue;
             }
 
-            var match1 = FileRegex().Match(line);
-            if (match1.Success)
-            {
-                if (file != null)
-                {
-                    sheet.Files.Add(file);
-                }
-
-                var name = match1.Groups[1].Value;
-                var type = match1.Groups[2].Value;
-                file = new CueSheetFile(name, type);
-                continue;
-            }
-
-            var match2 = TrackRegex().Match(line);
-            if (match2.Success)
-            {
-                var groups = match2.Groups;
-                var index = int.Parse(groups[1].Value);
-                var type = groups[2].Value;
-
-                track = new CueSheetTrack(index, type);
-
-                if (file is null)
-                {
-                    throw new InvalidOperationException();
-                }
-
-                file.Tracks.Add(track);
-                continue;
-            }
-
-            var match = FlagsRegex().Match(line);
-            if (match.Success)
-            {
-                if (track == null)
-                {
-                    throw new InvalidOperationException();
-                }
-
-                if (track.Flags.Count > 0)
-                {
-                    throw new InvalidDataException();
-                }
-
-                foreach (Capture c in match.Groups[1].Captures) // todo
-                {
-                    track.Flags.Add(c.Value);
-                }
-
-                continue;
-            }
-
-            var match3 = IndexRegex().Match(line);
-            if (match3.Success)
-            {
-                var groups = match3.Groups;
-                var number = int.Parse(groups[1].Value);
-                var m = int.Parse(groups[2].Value);
-                var s = int.Parse(groups[3].Value);
-                var f = int.Parse(groups[4].Value);
-                if (track is null)
-                {
-                    throw new InvalidOperationException();
-                }
-
-                var index = new CueSheetTrackIndex(number, m, s, f);
-
-                // todo check contiguous?
-                track.Indices.Add(index);
-                continue;
-            }
-
-            var match4 = RemRegex().Match(line);
-            if (match4.Success)
-            {
-                sheet.Comments.Add(match4.Groups[1].Value);
-                continue;
-            }
-
-            var match5 = CatalogRegex().Match(line);
-            if (match5.Success)
-            {
-                if (sheet.Catalog != null)
-                {
-                    throw new InvalidDataException();
-                }
-
-                sheet.Catalog = ulong.Parse(match5.Groups[1].Value);
-                continue;
-            }
-
-            var titleRegex = TitleRegex().Match(line);
-            if (titleRegex.Success)
-            {
-                var value = titleRegex.Groups[1].Value;
-
-                if (track != null)
-                {
-                    if (track.Title != null)
-                    {
-                        throw new InvalidDataException();
-                    }
-
-                    track.Title = value;
-                    continue;
-                }
-
-                if (sheet.Title != null)
-                {
-                    throw new InvalidDataException();
-                }
-
-                sheet.Title = value;
-                continue;
-            }
-
-            var performerRegex = PerformerRegex().Match(line);
-            if (performerRegex.Success)
-            {
-                var performer = performerRegex.Groups[1].Value;
-                if (track != null)
-                {
-                    if (track.Performer != null)
-                    {
-                        throw new InvalidDataException();
-                    }
-
-                    track.Performer = performer;
-                    continue;
-                }
-
-                if (sheet.Performer != null)
-                {
-                    throw new InvalidDataException();
-                }
-
-                sheet.Performer = performer;
-                continue;
-            }
-
-            var preGapRegex = PreGapRegex().Match(line);
-            if (preGapRegex.Success)
-            {
-                var m = byte.Parse(preGapRegex.Groups[1].Value);
-                var s = byte.Parse(preGapRegex.Groups[2].Value);
-                var f = byte.Parse(preGapRegex.Groups[3].Value);
-                var msf = new Msf(m, s, f);
-                if (track == null)
-                {
-                    throw new InvalidOperationException();
-                }
-
-                if (track.PreGap != null)
-                {
-                    throw new InvalidDataException();
-                }
-
-                track.PreGap = msf;
-                continue;
-            }
-
-            var isrcRegex = IsrcRegex().Match(line);
-            if (isrcRegex.Success)
-            {
-                if (track == null)
-                {
-                    throw new InvalidOperationException();
-                }
-
-                if (track.Isrc != null)
-                {
-                    throw new InvalidDataException();
-                }
-
-                track.Isrc = isrcRegex.Groups[1].Value;
-                continue;
-            }
-
             if (line.StartsWith(';'))
             {
-                continue; // non-compliant
+                continue; // non-compliant comment
+            }
+
+            var handled = false;
+
+            foreach (var (regex, handler) in Handlers)
+            {
+                if (!handler(regex, line, sheet, ref file, ref track))
+                {
+                    continue;
+                }
+
+                handled = true;
+                break;
+            }
+
+            if (handled)
+            {
+                continue;
             }
 
             throw new NotSupportedException(line);
@@ -249,4 +104,273 @@ public static partial class CueSheetParser
 
     [GeneratedRegex("""^\s*ISRC\s+(\w{5}\d{7})\s*\r?$""")]
     private static partial Regex IsrcRegex();
+
+    private static bool CatalogHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        if (sheet.Catalog != null)
+        {
+            throw new InvalidDataException();
+        }
+
+        var catalog = ulong.Parse(match.Groups[1].Value);
+
+        sheet.Catalog = catalog;
+
+        return true;
+    }
+
+    private static bool FileHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        if (file != null)
+        {
+            sheet.Files.Add(file);
+        }
+
+        var name = match.Groups[1].Value;
+        var type = match.Groups[2].Value;
+
+        file = new CueSheetFile(name, type);
+
+        return true;
+    }
+
+    private static bool FlagsHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        if (track is null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (track.Flags.Count > 0)
+        {
+            throw new InvalidDataException();
+        }
+
+        foreach (var capture in match.Groups[1].Captures.Cast<Capture>()) // TODO
+        {
+            track.Flags.Add(capture.Value);
+        }
+
+        return true;
+    }
+
+    private static bool IndexHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        if (track is null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var i = int.Parse(match.Groups[1].Value);
+        var m = int.Parse(match.Groups[2].Value);
+        var s = int.Parse(match.Groups[3].Value);
+        var f = int.Parse(match.Groups[4].Value);
+
+        var index = new CueSheetTrackIndex(i, m, s, f);
+
+        track.Indices.Add(index); // TODO check that these are contiguous?
+
+        return true;
+    }
+
+    private static bool PerformerHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        var performer = match.Groups[1].Value;
+
+        if (track != null)
+        {
+            if (track.Performer != null)
+            {
+                throw new InvalidDataException();
+            }
+
+            track.Performer = performer;
+
+            return true;
+        }
+
+        if (sheet.Performer != null)
+        {
+            throw new InvalidDataException();
+        }
+
+        sheet.Performer = performer;
+
+        return true;
+    }
+
+    private static bool PreGapHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        if (track == null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (track.PreGap != null)
+        {
+            throw new InvalidDataException();
+        }
+
+        var m = byte.Parse(match.Groups[1].Value);
+        var s = byte.Parse(match.Groups[2].Value);
+        var f = byte.Parse(match.Groups[3].Value);
+
+        track.PreGap = new Msf(m, s, f);
+
+        return true;
+    }
+
+    private static bool RemHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        var comment = match.Groups[1].Value;
+
+        sheet.Comments.Add(comment);
+
+        return true;
+    }
+
+    private static bool TitleHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        var title = match.Groups[1].Value;
+
+        if (track != null)
+        {
+            if (track.Title != null)
+            {
+                throw new InvalidDataException();
+            }
+
+            track.Title = title;
+
+            return true;
+        }
+
+        if (sheet.Title != null)
+        {
+            throw new InvalidDataException();
+        }
+
+        sheet.Title = title;
+
+        return true;
+    }
+
+    private static bool TrackHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        if (file is null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var index = int.Parse(match.Groups[1].Value);
+        var type = match.Groups[2].Value; // TODO
+
+        track = new CueSheetTrack(index, type);
+
+        file.Tracks.Add(track);
+
+        return true;
+    }
+
+    private static bool IsrcHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track)
+    {
+        var match = regex.Match(input);
+
+        if (match.Success == false)
+        {
+            return false;
+        }
+
+        if (track == null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (track.Isrc != null)
+        {
+            throw new InvalidDataException();
+        }
+
+        var isrc = match.Groups[1].Value;
+
+        track.Isrc = isrc;
+
+        return true;
+    }
+
+    private delegate bool CueSheetHandler(
+        Regex regex, string input, CueSheet sheet, ref CueSheetFile? file, ref CueSheetTrack? track);
 }
