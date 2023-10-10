94bbfbba0a5875d55438942c8e3c7e175cf19e89
aybe <aybe@users.noreply.github.com>
2023-10-10 23:58:10 +0200

    Implement ISector for audio and mode 2 form-less sectors

--- WipeoutInstaller/WorkInProgress/SectorAudio.cs
+++ WipeoutInstaller/WorkInProgress/SectorAudio.cs
@@ -1,6 +1,28 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace WipeoutInstaller.WorkInProgress;
 
-public unsafe struct SectorAudio
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public unsafe struct SectorAudio : ISector
 {
-    public fixed byte Data[2352];
+    public const int DataPosition = 0;
+
+    public const int DataSize = 2352;
+
+    public fixed byte Data[DataSize];
+
+    public uint GetEdc()
+    {
+        throw new NotSupportedException();
+    }
+
+    public uint GetEdcSum()
+    {
+        throw new NotSupportedException();
+    }
+
+    public Span<byte> GetUserData()
+    {
+        return ISector.GetSlice(ref this, DataPosition, DataSize);
+    }
 }
--- WipeoutInstaller/WorkInProgress/SectorMode2Formless.cs
+++ WipeoutInstaller/WorkInProgress/SectorMode2Formless.cs
@@ -1,8 +1,40 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace WipeoutInstaller.WorkInProgress;
 
-public unsafe struct SectorMode2Formless
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public unsafe struct SectorMode2Formless : ISector
 {
-    public fixed byte Sync[12];
-    public fixed byte Header[4];
-    public fixed byte UserData[2336];
+    public const int SyncPosition = 0;
+
+    public const int SyncSize = 12;
+
+    public fixed byte Sync[SyncSize];
+
+    public const int HeaderPosition = SyncPosition + SyncSize;
+
+    public const int HeaderSize = 4;
+
+    public fixed byte Header[HeaderSize];
+
+    public const int UserDataPosition = HeaderPosition + HeaderSize;
+
+    public const int UserDataSize = 2336;
+
+    public fixed byte UserData[UserDataSize];
+
+    public uint GetEdc()
+    {
+        throw new NotSupportedException();
+    }
+
+    public uint GetEdcSum()
+    {
+        throw new NotSupportedException();
+    }
+
+    public Span<byte> GetUserData()
+    {
+        return ISector.GetSlice(ref this, UserDataPosition, UserDataSize);
+    }
 }
