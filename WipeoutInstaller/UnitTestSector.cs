35b17c076a820d32e27bb23e29768f7c0df8943f
aybe <aybe@users.noreply.github.com>
2023-10-13 09:40:58 +0200

    Simplify sector size testing

--- WipeoutInstaller/UnitTestSector.cs
+++ WipeoutInstaller/UnitTestSector.cs
@@ -148,51 +148,17 @@ public class UnitTestSector : UnitTestBase
     }
 
     [TestMethod]
-    public void TestSectorSizeAudio()
-    {
-        TestSectorSize<SectorAudio>();
-    }
-
-    [TestMethod]
-    public void TestSectorSizeMode0()
-    {
-        TestSectorSize<SectorMode0>();
-    }
-
-    [TestMethod]
-    public void TestSectorSizeMode1()
-    {
-        TestSectorSize<SectorMode1>();
-    }
-
-    [TestMethod]
-    public void TestSectorSizeMode2Form1()
-    {
-        TestSectorSize<SectorMode2Form1>();
-    }
-
-    [TestMethod]
-    public void TestSectorSizeMode2Form2()
-    {
-        TestSectorSize<SectorMode2Form2>();
-    }
-
-    [TestMethod]
-    public void TestSectorSizeMode2FormLess()
-    {
-        TestSectorSize<SectorMode2FormLess>();
-    }
-
-    private static void TestSectorSize<T>() where T : struct, ISector
-    {
-        Assert.AreEqual(ISector.Size, Unsafe.SizeOf<T>());
-    }
-
-    [TestMethod]
     public void TestSizeOf()
     {
         Assert.AreEqual(3, Unsafe.SizeOf<SectorAddress>());
         Assert.AreEqual(4, Unsafe.SizeOf<SectorSubHeader>());
         Assert.AreEqual(8, Unsafe.SizeOf<SectorSubHeaderBlock>());
+
+        Assert.AreEqual(ISector.Size, Unsafe.SizeOf<SectorAudio>());
+        Assert.AreEqual(ISector.Size, Unsafe.SizeOf<SectorMode0>());
+        Assert.AreEqual(ISector.Size, Unsafe.SizeOf<SectorMode1>());
+        Assert.AreEqual(ISector.Size, Unsafe.SizeOf<SectorMode2Form1>());
+        Assert.AreEqual(ISector.Size, Unsafe.SizeOf<SectorMode2Form2>());
+        Assert.AreEqual(ISector.Size, Unsafe.SizeOf<SectorMode2FormLess>());
     }
 }
