d417198abbe58c466da69775a3357c55d326c4ac
aybe <aybe@users.noreply.github.com>
2023-10-15 02:43:15 +0200

    Make GetPosition* methods static, we'll need them for GetLength

--- WipeoutInstaller/WorkInProgress/DiscTrackCueBin.cs
+++ WipeoutInstaller/WorkInProgress/DiscTrackCueBin.cs
@@ -63,33 +63,38 @@ internal sealed class DiscTrackCueBin : DiscTrack
 
     public override int GetPosition() // pretty complex non-sense
     {
-        var files = CueSheetTrack.File.Sheet.Files;
+        return GetPosition(CueSheetTrack, CueSheetDirectory);
+    }
 
-        var list = new LinkedList<CueSheetTrack>(files.SelectMany(s => s.Tracks));
+    private static int GetPosition(CueSheetTrack track, string directory)
+    {
+        var files = track.File.Sheet.Files;
 
-        var position = files.Count is 1 ? GetPositionSingleFile(list) : GetPositionMultiFile(list);
+        var tracks = new LinkedList<CueSheetTrack>(files.SelectMany(s => s.Tracks));
 
-        position = GetPositionEcma130(list, position);
+        var position = files.Count is 1 ? GetPositionSingleFile(track, tracks) : GetPositionMultiFile(track, tracks, directory);
+
+        position = GetPositionEcma130(track, tracks, position);
 
         return position;
     }
 
-    private int GetPositionEcma130(LinkedList<CueSheetTrack> list, int position)
+    private static int GetPositionEcma130(CueSheetTrack track, LinkedList<CueSheetTrack> tracks, int position)
     {
-        for (var node = list.First; node != null; node = node.Next)
+        for (var node = tracks.First; node != null; node = node.Next)
         {
-            var track = node.Value;
+            var value = node.Value;
 
-            var a = track.Index is 1;
-            var b = track.Index is not 1 && node.Previous is { } previous && previous.Value.Type != track.Type;
-            var c = track.Type is not CueSheetTrackType.Audio && node.Next is { Value.Type: CueSheetTrackType.Audio };
+            var a = value.Index is 1;
+            var b = value.Index is not 1 && node.Previous is { } previous && previous.Value.Type != value.Type;
+            var c = value.Type is not CueSheetTrackType.Audio && node.Next is { Value.Type: CueSheetTrackType.Audio };
 
             if (a || b || c) // 20.2 User Data Area
             {
                 position += 150;
             }
 
-            if (track == CueSheetTrack)
+            if (value == track)
             {
                 break;
             }
@@ -98,51 +103,56 @@ internal sealed class DiscTrackCueBin : DiscTrack
         return position;
     }
 
-    private int GetPositionMultiFile(LinkedList<CueSheetTrack> list)
+    private static int GetPositionMultiFile(CueSheetTrack track, LinkedList<CueSheetTrack> tracks, string directory)
     {
         var position = -150; // pre-gap
 
-        for (var node = list.First; node != null; node = node.Next)
+        for (var node = tracks.First; node != null; node = node.Next)
         {
-            var track = node.Value;
+            var value = node.Value;
 
-            if (track == CueSheetTrack)
+            if (value == track)
             {
                 break;
             }
 
-            var path = Path.Combine(CueSheetDirectory, track.File.Name);
-            var info = new FileInfo(path);
-            var size = info.Length / ISector.Size;
+            var path = Path.Combine(directory, value.File.Name);
 
-            position += (int)size;
+            var info = new FileInfo(path);
+
+            var bytes = info.Length;
+
+            var sectors = bytes / ISector.Size;
+            var length = Convert.ToInt32(sectors);
+
+            position += length;
         }
 
         return position;
     }
 
-    private int GetPositionSingleFile(LinkedList<CueSheetTrack> list)
+    private static int GetPositionSingleFile(CueSheetTrack track, LinkedList<CueSheetTrack> tracks)
     {
         var position = 0;
 
-        for (var node = list.First; node != null; node = node.Next)
+        for (var node = tracks.First; node != null; node = node.Next)
         {
-            var track = node.Value;
+            var value = node.Value;
 
-            var index0 = track.Indices.SingleOrDefault(s => s.Number is 0);
-            var index1 = track.Indices.Single(s => s.Number is 1);
+            var index0 = value.Indices.SingleOrDefault(s => s.Number is 0);
+            var index1 = value.Indices.Single(s => s.Number is 1);
 
             position = index1.Position.ToLBA(); // absolute position
 
             if (index0 is not null)
             {
-                if (track.Index is not 1) // pre-gap
+                if (value.Index is not 1) // pre-gap
                 {
                     position -= position - index0.Position.ToLBA();
                 }
             }
 
-            if (track == CueSheetTrack)
+            if (value == track)
             {
                 break;
             }
