3aad9fa1bb227730e88697a05c240188f3060a65
aybe <aybe@users.noreply.github.com>
2023-10-17 10:59:52 +0200

    Get that NetBSD image to pass...

--- WipeoutInstaller/ISO9660/IsoFileSystemEntryFile.cs
+++ WipeoutInstaller/ISO9660/IsoFileSystemEntryFile.cs
@@ -7,7 +7,14 @@ public sealed partial class IsoFileSystemEntryFile : IsoFileSystemEntry
     public IsoFileSystemEntryFile(IsoFileSystemEntryDirectory? parent, DirectoryRecord record)
         : base(parent, record)
     {
-        Version = Convert.ToInt32(VersionRegex().Match(record.FileIdentifier).Value);
+        var regex = VersionRegex();
+
+        var match = regex.Match(record.FileIdentifier);
+
+        if (match.Success) // BUG who is wrong, NetBSD or ECMA-130?
+        {
+            Version = Convert.ToInt32(match.Value);
+        }
     }
 
     public int Length => Record.DataLength;
