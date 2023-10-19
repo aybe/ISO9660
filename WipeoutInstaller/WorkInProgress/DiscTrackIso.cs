f8a42e2716b2c88e6360ba0ffbde68913c52bb75
aybe <aybe@users.noreply.github.com>
2023-10-19 02:51:55 +0200

    Initialize DiscTrackIso.Sector in constructor

--- WipeoutInstaller/WorkInProgress/DiscTrackIso.cs
+++ WipeoutInstaller/WorkInProgress/DiscTrackIso.cs
@@ -4,7 +4,16 @@ public sealed class DiscTrackIso : DiscTrack
 {
     public DiscTrackIso(Stream stream)
     {
-        Stream = stream; // TODO GetSector here, throw immediately if wrong
+        ISector[] sectors =
+        {
+            new SectorCooked2048(),
+            new SectorCooked2324(),
+            new SectorCooked2336()
+        };
+
+        Sector = sectors.Single(s => stream.Length % s.GetUserDataLength() == 0);
+
+        Stream = stream;
     }
 
     private Stream Stream { get; }
@@ -27,35 +36,7 @@ public sealed class DiscTrackIso : DiscTrack
 
     public override int Position { get; } = 0; // TODO ?
 
-    public override ISector Sector
-    {
-        get
-        {
-            var length = Stream.Length;
-
-            const int size2048 = SectorCooked2048.UserDataSize;
-            const int size2324 = SectorCooked2324.UserDataSize;
-            const int size2336 = SectorCooked2336.UserDataSize;
-
-            var size = true switch
-            {
-                true when length % size2048 is 0 => size2048,
-                true when length % size2324 is 0 => size2324,
-                true when length % size2336 is 0 => size2336,
-                _                                => throw new NotSupportedException()
-            };
-
-            ISector sector = size switch
-            {
-                size2048 => new SectorCooked2048(),
-                size2324 => new SectorCooked2324(),
-                size2336 => new SectorCooked2336(),
-                _        => throw new NotSupportedException()
-            };
-
-            return sector;
-        }
-    }
+    public override ISector Sector { get; }
 
     public override ISector ReadSector(in int index)
     {
