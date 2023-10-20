53295a6e882e346cf90e88e7afdddb3dcae81f5f
aybe <aybe@users.noreply.github.com>
2023-10-20 08:58:34 +0200

    Move read file test to own unit test

---
+++ WipeoutInstaller/UnitIsoTestReadFile.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using WipeoutInstaller.ISO9660;
+using WipeoutInstaller.Templates;
+
+namespace WipeoutInstaller;
+
+[TestClass]
+public sealed class UnitIsoTestReadFile : UnitTestDisc
+{
+    public static IEnumerable<object[]> TestIsoReadFileInit()
+    {
+        var files = TestData.GetCsvTestData<TestDataIsoReadFile>(@"Templates\TestDataIsoReadFile.csv");
+
+        foreach (var file in files)
+        {
+            yield return new object[] { file.Source, file.Target, file.Sha256, file.Mode };
+        }
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(TestIsoReadFileInit), DynamicDataSourceType.Method)]
+    public void TestIsoReadFile(string source, string target, string sha256, DiscReadFileMode mode)
+    {
+        using var disc = LoadDiscFromCue(source);
+
+        var ifs = IsoFileSystem.Read(disc);
+
+        var tryFindFile = ifs.TryFindFile(target, out var result);
+
+        Assert.IsTrue(tryFindFile);
+
+        Assert.IsNotNull(result);
+
+        using var stream = new MemoryStream();
+
+        disc.ReadFile(result, mode, stream);
+
+        stream.Position = 0;
+
+        var hashData = SHA256.HashData(stream);
+
+        var hashText = string.Concat(hashData.Select(s => s.ToString("x2")));
+
+        Assert.AreEqual(sha256, hashText, StringComparer.OrdinalIgnoreCase);
+    }
+}
--- WipeoutInstaller/UnitTestDisc.cs
+++ WipeoutInstaller/UnitTestDisc.cs
@@ -1,8 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Security.Cryptography;
 using System.Text;
 using WipeoutInstaller.ISO9660;
-using WipeoutInstaller.Templates;
 using WipeoutInstaller.WorkInProgress;
 
 namespace WipeoutInstaller;
@@ -146,43 +144,6 @@ public class UnitTestDisc : UnitTestBase
             }
         }
     };
-
-    public static IEnumerable<object[]> TestIsoReadFileInit()
-    {
-        var files = TestData.GetCsvTestData<TestDataIsoReadFile>(@"Templates\TestDataIsoReadFile.csv");
-
-        foreach (var file in files)
-        {
-            yield return new object[] { file.Source, file.Target, file.Sha256, file.Mode };
-        }
-    }
-
-    [TestMethod]
-    [DynamicData(nameof(TestIsoReadFileInit), DynamicDataSourceType.Method)]
-    public void TestIsoReadFile(string source, string target, string sha256, DiscReadFileMode mode)
-    {
-        using var disc = LoadDiscFromCue(source);
-
-        var ifs = IsoFileSystem.Read(disc);
-
-        var tryFindFile = ifs.TryFindFile(target, out var result);
-
-        Assert.IsTrue(tryFindFile);
-
-        Assert.IsNotNull(result);
-
-        using var stream = new MemoryStream();
-
-        disc.ReadFile(result, mode, stream);
-
-        stream.Position = 0;
-
-        var hashData = SHA256.HashData(stream);
-
-        var hashText = string.Concat(hashData.Select(s => s.ToString("x2")));
-
-        Assert.AreEqual(sha256, hashText, StringComparer.OrdinalIgnoreCase);
-    }
 
     [TestMethod]
     [DataRow(@"D:\Temp\CD-I Demo Disc - Fall 1996 - Spring 1997.cue")]
