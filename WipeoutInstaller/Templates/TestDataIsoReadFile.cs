c3f51772546cadb95d7c12720a06bd6fac8af807
aybe <aybe@users.noreply.github.com>
2023-10-20 08:51:05 +0200

    Add dedicated read file test

---
+++ WipeoutInstaller/Templates/TestDataIsoReadFile.cs
@@ -0,0 +1,3 @@
+﻿namespace WipeoutInstaller.Templates;
+
+public record TestDataIsoReadFile(string Source, string Target, string Sha256, DiscReadFileMode Mode) : TestData;
--- WipeoutInstaller/UnitTestDisc.cs
+++ WipeoutInstaller/UnitTestDisc.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
 using System.Text;
 using WipeoutInstaller.ISO9660;
 using WipeoutInstaller.Templates;
@@ -174,6 +175,43 @@ public class UnitTestDisc : UnitTestBase
         }
 
         WriteLine(() => result);
+    }
+
+    public static IEnumerable<object[]> TestIsoReadFileInit()
+    {
+        var files = TestData.GetCsvTestData<TestDataIsoReadFile>(@"Templates\TestDataIsoReadFile.csv");
+
+        foreach (var file in files)
+        {
+            yield return new object[] { file.Source, file.Target, file.Sha256, file.Mode };
+        }
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(TestIsoReadFileInit), DynamicDataSourceType.Method)]
+    public void TestIsoReadFile(string source, string target, string sha256, DiscReadFileMode mode)
+    {
+        using var disc = LoadDiscFromCue(source);
+
+        var ifs = IsoFileSystem.Read(disc);
+
+        var tryFindFile = ifs.TryFindFile(target, out var result);
+
+        Assert.IsTrue(tryFindFile);
+
+        Assert.IsNotNull(result);
+
+        using var stream = new MemoryStream();
+
+        disc.ReadFile(result, mode, stream);
+
+        stream.Position = 0;
+
+        var hashData = SHA256.HashData(stream);
+
+        var hashText = string.Concat(hashData.Select(s => s.ToString("x2")));
+
+        Assert.AreEqual(sha256, hashText, StringComparer.OrdinalIgnoreCase);
     }
 
     [TestMethod]
