d062894c37fa3f8260eb7b1b9a0514102101122e
aybe <aybe@users.noreply.github.com>
2023-10-26 06:58:33 +0200

    Remove useless extensions

--- ISO9660.Tests/Extensions/ArrayExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace ISO9660.Tests.Extensions;
-
-public static class ArrayExtensions
-{
-    public static BinaryReader ToBinaryReader(this byte[] array)
-    {
-        return new BinaryReader(new MemoryStream(array));
-    }
-}
--- ISO9660.Tests/Extensions/SpanExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ISO9660.Tests.Extensions;
-
-public static class SpanExtensions
-{
-    public static BinaryReader ToBinaryReader(this Span<byte> span)
-    {
-        return new BinaryReader(new MemoryStream(span.ToArray()));
-    }
-}
