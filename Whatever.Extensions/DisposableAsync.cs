9dbd750ea61cd486ac8f0e7341e00a30343c6f3f
aybe <aybe@users.noreply.github.com>
2023-11-06 08:47:13 +0100

    Create DisposableAsync.cs

---
+++ Whatever.Extensions/DisposableAsync.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Whatever.Extensions
+{
+    public abstract class DisposableAsync : Disposable
+    {
+        protected virtual async ValueTask DisposeAsyncCore()
+        {
+            await new ValueTask().ConfigureAwait(false);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsyncCore();
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
