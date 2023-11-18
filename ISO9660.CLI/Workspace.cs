c44c9e70abfef8210ba485f4d29415ba79eaf685
aybe <aybe@users.noreply.github.com>
2023-11-18 07:39:49 +0100

    CLI: extract workspace to own file

---
+++ ISO9660.CLI/Workspace.cs
@@ -0,0 +1,60 @@
+﻿using ISO9660.Logical;
+using ISO9660.Physical;
+using Whatever.Extensions;
+
+namespace ISO9660.CLI;
+
+internal sealed class Workspace : Disposable
+{
+    private Workspace()
+    {
+    }
+
+    public Disc? Disc { get; private set; }
+
+    public IsoFileSystem? System { get; private set; }
+
+    protected override void DisposeManaged()
+    {
+        System?.Dispose();
+
+        Disc?.Dispose();
+    }
+
+    public static Workspace TryOpen(string path)
+    {
+        var workspace = new Workspace();
+
+        if (!File.Exists(path))
+        {
+            throw new InvalidOperationException($"Image file could not be found: '{path}'.");
+        }
+
+        var extension = Path.GetExtension(path).ToLowerInvariant();
+
+        try
+        {
+            workspace.Disc = extension switch
+            {
+                ".cue" => Disc.FromCue(path),
+                ".iso" => Disc.FromIso(path),
+                _      => throw new NotSupportedException($"Image file type is not supported: '{extension}'.")
+            };
+        }
+        catch (Exception e)
+        {
+            throw new InvalidOperationException("Disc could not be read from image file.", e);
+        }
+
+        try
+        {
+            workspace.System = IsoFileSystem.Read(workspace.Disc);
+        }
+        catch (Exception e)
+        {
+            throw new InvalidOperationException("File system could not be read from disc.", e);
+        }
+
+        return workspace;
+    }
+}
