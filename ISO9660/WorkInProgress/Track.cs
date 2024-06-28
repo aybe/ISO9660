fed99432883f5f09c173b43fc1f6f08d2b7d1a3a
aybe <aybe@users.noreply.github.com>
2024-06-28 20:36:39 +0200

    Make track record as class

--- ISO9660/WorkInProgress/Track.cs
+++ ISO9660/WorkInProgress/Track.cs
@@ -1,3 +1,12 @@
 ﻿namespace ISO9660.WorkInProgress;
 
-public sealed record Track(byte Number, int Position, int Length, TrackType Type);
+public sealed class Track(byte Number, int Position, int Length, TrackType Type)
+{
+    public byte Number { get; } = Number;
+
+    public int Position { get; } = Position;
+
+    public int Length { get; } = Length;
+
+    public TrackType Type { get; } = Type;
+}
