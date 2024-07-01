d8708633939049859c539088a1bd4ddc06d6fe72
aybe <aybe@users.noreply.github.com>
2024-07-01 13:06:44 +0200

    Test that raw/user file read works on real drive (it does)

--- ISO9660.Tests/UnitTestRealDevice.cs
+++ ISO9660.Tests/UnitTestRealDevice.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography;
+using System.Text;
 using ISO9660.Logical;
 using ISO9660.Physical;
 
@@ -8,11 +9,11 @@ namespace ISO9660.Tests;
 public sealed class UnitTestRealDevice : UnitTestBase
 {
     [TestMethod]
-    public void TestMethod1()
+    public async Task TestMethod1()
     {
         var info = GetDrive();
 
-        using var disc = Disc.Open(info.Name);
+        await using var disc = Disc.Open(info.Name);
 
         foreach (var track in disc.Tracks)
         {
@@ -20,6 +21,28 @@ public sealed class UnitTestRealDevice : UnitTestBase
         }
 
         var fs = IsoFileSystem.Read(disc);
+
+        if (fs.TryFindFile("/SCES_000.10", out var file1))
+        {
+            using var stream = new MemoryStream();
+            await disc.ReadFileUserAsync(file1, stream);
+            stream.Position = 0;
+            var hash = await SHA1.HashDataAsync(stream);
+            const string expected = "5ca9383a1b988baef91fe4ca686855dc0e70db74";
+            var actual = string.Concat(hash.Select(s => s.ToString("x2")));
+            Assert.AreEqual(expected, actual, true);
+        }
+
+        if (fs.TryFindFile("/WOPAL.AV", out var file2))
+        {
+            using var stream = new MemoryStream();
+            await disc.ReadFileRawAsync(file2, stream);
+            stream.Position = 0;
+            var hash = await SHA1.HashDataAsync(stream);
+            const string expected = "ea236c70fe7c704a62fbe88f46c2e3e12acdf8c9";
+            var actual = string.Concat(hash.Select(s => s.ToString("x2")));
+            Assert.AreEqual(expected, actual, true);
+        }
 
         var builder = new StringBuilder();
 
